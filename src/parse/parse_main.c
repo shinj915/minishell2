78fbd33772e42418b43389c3cef59ef872f2c51d
Jisu Shin <jishin@c1r23s6.42gyeongsan.kr>
2025-04-14 17:05:54 +0900

    feat: add delete_space_token

--- src/parse/parse_main.c
+++ src/parse/parse_main.c
@@ -6,11 +6,36 @@
 /*   By: jishin <jishin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 14:20:14 by jishin            #+#    #+#             */
-/*   Updated: 2025/04/14 16:02:30 by jishin           ###   ########.fr       */
+/*   Updated: 2025/04/14 17:04:49 by jishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+static void	delete_space_token(t_token **token)
+{
+	t_token	*ptr;
+	t_token	*prev;
+	t_token	*next;
+
+	ptr = *token;
+	prev = NULL;
+	while (ptr != NULL)
+	{
+		next = ptr->next;
+		if (ptr->token_type == TYPE_TOKEN_SPACE)
+		{
+			if (prev != NULL)
+				prev->next = next;
+			else
+				*token = next;
+			free_token(ptr);
+		}
+		else
+			prev = ptr;
+		ptr = next;
+	}
+}
 
 t_token	*tokenize(char *cmd, t_state *state)
 {
@@ -25,7 +50,8 @@ t_token	*tokenize(char *cmd, t_state *state)
 	tokenize_pipe(result);
 	tokenize_redirect(result);
 	delete_empty_token(&result);
-	tokenize_chunk_to_argv(result);	
+	tokenize_chunk_to_argv(result);
+	delete_space_token(&result);
 	return (result);
 }
 
