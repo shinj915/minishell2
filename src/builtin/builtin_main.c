8c06d3ff21ce4a58a62781ab898f2d1725887514
eunam <yu21iarty@gmail.com>
2025-05-06 21:34:51 +0900

    fix builtin_main

--- src/builtin/builtin_main.c
+++ src/builtin/builtin_main.c
@@ -6,31 +6,26 @@
 /*   By: eunam <eunam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:27:56 by eunam             #+#    #+#             */
-/*   Updated: 2025/05/06 13:27:57 by eunam            ###   ########.fr       */
+/*   Updated: 2025/05/06 21:34:22 by eunam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	is_builtin_command(t_cmd_list *cmd_list, t_state *state)
+void	is_builtin_command(t_cmd *cmd, t_state *state)
 {
-	char	*builtins[] = {"echo", "cd", "pwd", "export", "unset", "env", "exit"};
-	t_cmd	*recent_cmd;
-
-	recent_cmd = cmd_list->head;
-	if (ft_strcmp(recent_cmd->argv[0], builtins[0]) == 0)
-		builtin_execute_echo(recent_cmd, state);
-	else if (ft_strcmp(recent_cmd->argv[0], builtins[1]) == 0)
-		builtin_execute_cd(recent_cmd, state);
-	else if (ft_strcmp(recent_cmd->argv[0], builtins[2]) == 0)
-		builtin_execute_pwd(recent_cmd, state);
-	else if (ft_strcmp(recent_cmd->argv[0], builtins[3]) == 0)
-		builtin_execute_export(recent_cmd, state);
-	else if (ft_strcmp(recent_cmd->argv[0], builtins[4]) == 0)
-		builtin_execute_unset(recent_cmd, state);
-	else if (ft_strcmp(recent_cmd->argv[0], builtins[5]) == 0)
-		builtin_execute_env(recent_cmd, state);
-	else if (ft_strcmp(recent_cmd->argv[0], builtins[6]) == 0)
-		builtin_execute_exit(recent_cmd, state);
-	// add_history(state->cmd_list);
+	if (ft_strcmp(cmd->argv[0], "echo") == 0)
+		builtin_execute_echo(cmd, state);
+	else if (ft_strcmp(cmd->argv[0], "cd") == 0)
+		builtin_execute_cd(cmd, state);
+	else if (ft_strcmp(cmd->argv[0], "pwd") == 0)
+		builtin_execute_pwd(cmd, state);
+	else if (ft_strcmp(cmd->argv[0], "export") == 0)
+		builtin_execute_export(cmd, state);
+	else if (ft_strcmp(cmd->argv[0], "unset") == 0)
+		builtin_execute_unset(cmd, state);
+	else if (ft_strcmp(cmd->argv[0], "env") == 0)
+		builtin_execute_env(cmd, state);
+	else if (ft_strcmp(cmd->argv[0], "exit") == 0)
+		builtin_execute_exit(cmd, state);
 }
